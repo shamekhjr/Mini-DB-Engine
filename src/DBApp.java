b6cc976bcca2e283fe015fc87f1b8c96fb60cf4f
Mohamed Shamekh <shamekhjr@gmail.com>
2023-03-14 11:45:42 +0200

    Added required method signatures (for M1)

--- src/DBApp.java
+++ src/DBApp.java
@@ -1,2 +1,67 @@
+import java.util.Hashtable;
+import java.util.Iterator;
+
 public class DBApp {
+    public void init( ) {
+        //very bri'ish amirit
+    }
+
+
+    // following method creates one table only
+    // strClusteringKeyColumn is the name of the column that will be the primary
+    // key and the clustering column as well. The data type of that column will
+    // be passed in htblColNameType
+    // htblColNameValue will have the column name as key and the data
+    // type as value
+    // htblColNameMin and htblColNameMax for passing minimum and maximum values
+    // for data in the column. Key is the name of the column
+    public void createTable(String strTableName, String strClusteringKeyColumn,
+                            Hashtable<String,String> htblColNameType, Hashtable<String,String> htblColNameMin,
+                            Hashtable<String,String> htblColNameMax ) throws DBAppException {
+
+    }
+
+
+    // following method inserts one row only.
+    // htblColNameValue must include a value for the primary key
+    public void insertIntoTable(String strTableName,
+                                Hashtable<String,Object> htblColNameValue) throws DBAppException {
+
+    }
+
+
+    // following method updates one row only
+    // htblColNameValue holds the key and new value
+    // htblColNameValue will not include clustering key as column name
+    // strClusteringKeyValue is the value to look for to find the row to update.
+    public void updateTable(String strTableName,
+                            String strClusteringKeyValue,
+                            Hashtable<String,Object> htblColNameValue )
+            throws DBAppException {
+
+    }
+
+
+    // following method could be used to delete one or more rows.
+    // htblColNameValue holds the key and value. This will be used in search
+    // to identify which rows/tuples to delete.
+    // htblColNameValue enteries are ANDED together
+    public void deleteFromTable(String strTableName,
+                                Hashtable<String,Object> htblColNameValue)
+            throws DBAppException {
+
+    }
+
+    public Iterator selectFromTable(SQLTerm[] arrSQLTerms,
+                                    String[] strarrOperators)
+            throws DBAppException {
+        return null;
+
+    }
+
+    //bonus
+    public Iterator parseSQL( StringBuffer strbufSQL ) throws
+            DBAppException {
+        return null;
+    }
 }
---
+++ src/SQLTerm.java
@@ -0,0 +1,19 @@
+public class SQLTerm {
+    String _strTableName;
+    String _strColumnName;
+    String _strOperator;
+    Object _objValue;
+
+    public SQLTerm(String strTableName, String strColumnName, String strOperator, Object objValue) {
+        _strTableName = strTableName;
+        _strColumnName = strColumnName;
+        _strOperator = strOperator;
+        _objValue = objValue;
+    }
+
+    //empty constructor
+    public SQLTerm() {
+    }
+
+
+}
