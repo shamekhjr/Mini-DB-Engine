654d30184d17502b4e90dbaff3fb29304fb57d84
Omar Nour <omar.syd.n@gmail.com>
2023-05-08 23:15:42 +0300

    Merge OctTree Code

--- src/main/java/OctTree.java
+++ src/main/java/OctTree.java
@@ -1,19 +1,33 @@
-import java.io.FileInputStream;
-import java.io.Serializable;
-import java.util.Hashtable;
-import java.util.Properties;
-import java.util.Vector;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class OctTree implements Serializable {
     OctTreeNode root;
     int maxEntries;
-    String[] colNames;
+    String[][] colNamesDatatypes;
     boolean onPrimaryKey;
+    String sTableName;
+    String sIndexName;
 
-    public OctTree(String col1Name, String col2Name, String col3Name, Comparable minC1, Comparable minC2, Comparable minC3, Comparable maxC1, Comparable maxC2, Comparable maxC3) throws DBAppException {
+    public OctTree(String sIndexName, String col1Name, String col2Name, String col3Name, String sTableName) throws DBAppException {
         this.maxEntries = loadMaxEntries();
-        root = new OctTreeNode(minC1, minC2, minC3, maxC1, maxC2, maxC3, maxEntries);
-        this.colNames = new String[] {col1Name, col2Name, col3Name};
+        this.sIndexName = sIndexName;
+        this.colNamesDatatypes = new String[3][2];
+        colNamesDatatypes[0][0] = col1Name;
+        colNamesDatatypes[1][0] = col2Name;
+        colNamesDatatypes[2][0] = col3Name;
+        this.sTableName = sTableName;
+        try {
+            Hashtable<String, Pair<Comparable, Comparable>> htblMinMax = rootMinMax();
+            root = new OctTreeNode(colNamesDatatypes, htblMinMax, maxEntries);
+        } catch (Exception e) {
+            throw new DBAppException(e);
+        }
         // TODO update metadata
     }
 
@@ -28,6 +42,97 @@ public class OctTree implements Serializable {
             throw new DBAppException(e);
         }
         return Integer.parseInt(configProperties.getProperty("MaximumEntriesinOctreeNode"));
+    }
+
+    public Hashtable<String, Pair<Comparable, Comparable>> rootMinMax () throws DBAppException, IOException, CsvValidationException, ParseException {
+        CSVReader reader = new CSVReader(new FileReader("src/main/java/metadata.csv"));
+        String[] line;
+        SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD", Locale.ENGLISH);
+        Hashtable<String, Pair<Object, Object>> hMinMaxPerColumn = new Hashtable<>();
+
+        while ((line = reader.readNext()) != null) {
+            if (line[0].equals()) {
+                if (line[1].equals(colNamesDatatypes[0][0])) {
+                    colNamesDatatypes[0][1] = line[2];
+                    if (colNamesDatatypes[0][1].equals("java.lang.Integer")) {
+                        Integer min = Integer.parseInt(line[7]);
+                        Integer max = Integer.parseInt(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[0][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[0][1].equals(" java.lang.String")) {
+                        String min = line[7];
+                        String max = line[8];
+                        hMinMaxPerColumn.put(colNamesDatatypes[0][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[0][1].equals("java.lang.Double")) {
+                        Double min = Double.parseDouble(line[7]);
+                        Double max = Double.parseDouble(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[0][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[0][1].equals("java.util.Date")) {
+                        Date min = formatter.parse(line[7]);
+                        Date max = formatter.parse(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[0][0],new Pair(min, max));
+                    }
+                    else {
+                        throw new DBAppException("Invalid data type: " + colNamesDatatypes[0][1]);
+                    }
+                }
+                if (line[1].equals(colNamesDatatypes[1][0])) {
+                    colNamesDatatypes[1][1] = line[2];
+                    if (colNamesDatatypes[1][1].equals("java.lang.Integer")) {
+                        Integer min = Integer.parseInt(line[7]);
+                        Integer max = Integer.parseInt(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[1][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[1][1].equals(" java.lang.String")) {
+                        String min = line[7];
+                        String max = line[8];
+                        hMinMaxPerColumn.put(colNamesDatatypes[1][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[1][1].equals("java.lang.Double")) {
+                        Double min = Double.parseDouble(line[7]);
+                        Double max = Double.parseDouble(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[1][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[1][1].equals("java.util.Date")) {
+                        Date min = formatter.parse(line[7]);
+                        Date max = formatter.parse(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[1][0],new Pair(min, max));
+                    }
+                    else {
+                        throw new DBAppException("Invalid data type: " + colNamesDatatypes[1][1]);
+                    }
+                }
+                if (line[1].equals(colNamesDatatypes[2][0])) {
+                    colNamesDatatypes[2][1] = line[2];
+                    if (colNamesDatatypes[2][1].equals("java.lang.Integer")) {
+                        Integer min = Integer.parseInt(line[7]);
+                        Integer max = Integer.parseInt(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[2][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[2][1].equals("java.lang.String")) {
+                        String min = line[7];
+                        String max = line[8];
+                        hMinMaxPerColumn.put(colNamesDatatypes[2][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[2][1].equals("java.lang.Double")) {
+                        Double min = Double.parseDouble(line[7]);
+                        Double max = Double.parseDouble(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[2][0],new Pair(min, max));
+                    }
+                    else if (colNamesDatatypes[2][1].equals("java.util.Date")) {
+                        Date min = formatter.parse(line[7]);
+                        Date max = formatter.parse(line[8]);
+                        hMinMaxPerColumn.put(colNamesDatatypes[2][0],new Pair(min, max));
+                    }
+                    else {
+                        throw new DBAppException("Invalid data type: " + colNamesDatatypes[2][1]);
+                    }
+                }
+            }
+        }
+        return hMinMaxPerColumn;
     }
 
     public void insert(Hashtable<String, Object> record, int page) throws DBAppException { // finds the not full wrapping node and inserts
@@ -49,6 +154,4 @@ public class OctTree implements Serializable {
         // TODO
         return null;
     }
-
-
 }
--- src/main/java/OctTreeNode.java
+++ src/main/java/OctTreeNode.java
@@ -1,51 +1,62 @@
 import java.io.Serializable;
+import java.util.Hashtable;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentMap;
 
 public class OctTreeNode  implements Serializable {
-    public Comparable minC1, minC2, minC3, maxC1, maxC2, maxC3;
     public Vector<Point> points;
     public OctTreeNode[] children;
-    String[] colNames;
+    String[][] colNamesDatatypes;
+    public Hashtable<String, Pair<Comparable, Comparable>> hMinMaxPerColumn;
     public int maxEntries;
+    boolean isLeaf;
 
-   public OctTreeNode(Comparable minC1, Comparable minC2, Comparable minC3, Comparable maxC1, Comparable maxC2, Comparable maxC3, int maxEntries) {
-        this.minC1 = minC1;
-        this.minC2 = minC2;
-        this.minC3 = minC3;
-        this.maxC1 = maxC1;
-        this.maxC2 = maxC2;
-        this.maxC3 = maxC3;
+   public OctTreeNode(String[][] colNamesDatatypes, Hashtable<String, Pair<Comparable, Comparable>> hMinMaxPerColumn, int maxEntries) {
+        this.colNamesDatatypes = colNamesDatatypes;
         this.maxEntries = maxEntries;
         this.points = new Vector<>(maxEntries);
         this.children = new OctTreeNode[8];
+        this.isLeaf = true;
+        this.hMinMaxPerColumn = hMinMaxPerColumn;
    }
 
    public boolean isFull() {
-        return points.size() == maxEntries;
+       return points.size() == maxEntries;
    }
 
    public boolean isEmpty() {
-        return points.size() == 0;
+       return points.size() == 0;
    }
 
-   public boolean wraps(Point entry) {
-        return (entry.col1.compareTo(minC1) >= 0 && entry.col1.compareTo(maxC1) <= 0) &&
-                (entry.col2.compareTo(minC2) >= 0 && entry.col2.compareTo(maxC2) <= 0) &&
-                (entry.col3.compareTo(minC3) >= 0 && entry.col3.compareTo(maxC3) <= 0);
+   public boolean wraps(Point p) {
+       // TODO accept less than 3 cols
+//        return  (p.col1.compareTo(hMinMaxPerColumn.get(colNamesDatatypes[0][0])) >= 0 && p.col1.compareTo(maxC1) <= 0) &&
+//                (p.col2.compareTo(minC2) >= 0 && p.col2.compareTo(maxC2) <= 0) &&
+//                (p.col3.compareTo(minC3) >= 0 && p.col3.compareTo(maxC3) <= 0);
+       return true;
    }
 
-   public void insert(Point p) { // finds the not full wrapping node and inserts, insert/subdivide-&-insert
-       if (this.wraps(p) && points.size() < maxEntries) {
-           points.add(p);
-       } else if (true) { // has children
-              // find child that wraps e
-              // insert in child
+   public void insert(Point p) { // finds the not full wrapping OctTreeNode and inserts, insert/subdivide-&-insert
+       if(!isLeaf) {
+           // find child that wraps e
+           // insert in child
        } else {
-           // TODO
-           subdivide();
-           distribute();
-           insertInChildren(p);
-           // insert in division
+           boolean duplicates = false;
+           if (this.wraps(p)) {
+               if (duplicates) { // check if duplicates
+
+               } else if (points.size() < maxEntries) { // check if space
+                   points.add(p);
+               } else {
+                   // create children
+                   // TODO
+                   subdivide();
+                   distribute();
+                   this.isLeaf = false;
+                   insert(p);
+                   // insert in division
+               }
+           }
        }
    }
 
@@ -74,4 +85,138 @@ public class OctTreeNode  implements Serializable {
    public void insertInChildren(Point p) {
 
    }
+
+    public Vector<OctTreeNode> search (Hashtable<String, Object> htblColNameValue) {
+        Comparable valColumn1 =  (Comparable) htblColNameValue.get(colNamesDatatypes[0][0]);
+        Comparable valColumn2 =  (Comparable) htblColNameValue.get(colNamesDatatypes[1][0]);
+        Comparable valColumn3 =  (Comparable) htblColNameValue.get(colNamesDatatypes[2][0]);
+
+        if (htblColNameValue.size() == 3) {
+            return searchThreeColumns(valColumn1,valColumn2,valColumn3);
+        }
+        else if (htblColNameValue.size() == 2) {
+            if (valColumn1 == null) {
+                return searchTwoColumns(new Pair(colNamesDatatypes[1][0], valColumn2), new Pair(colNamesDatatypes[2][0], valColumn3));
+            }
+            else  if (valColumn2 == null) {
+                return searchTwoColumns(new Pair(colNamesDatatypes[0][0], valColumn1), new Pair(colNamesDatatypes[1][0], valColumn3));
+            }
+            else {
+                return searchTwoColumns(new Pair(colNamesDatatypes[0][0], valColumn1), new Pair(colNamesDatatypes[1][0], valColumn2));
+            }
+        }
+        else if (htblColNameValue.size() ==1) {
+            if (valColumn1 != null) {
+                return searchOneColumn(new Pair(colNamesDatatypes[0][0], valColumn1));
+            }
+            else if (valColumn2 != null) {
+                return searchOneColumn(new Pair(colNamesDatatypes[1][0], valColumn2));
+            }
+            else if (valColumn3 != null) {
+                return searchOneColumn(new Pair(colNamesDatatypes[2][0], valColumn3));
+            }
+        }
+        return new Vector<OctTreeNode>(); // Empty list of OctTreeNodes
+    }
+
+    /*
+    If OctTreeNode is a leaf:
+       1. Return the current OctTreeNode in a vector
+    If OctTreeNode is a non-leaf:
+       1. Decide which child OctTreeNode to go next.
+    */
+    public Vector<OctTreeNode> searchThreeColumns (Comparable valCol1, Comparable valCol2, Comparable valCol3) {
+        if (isLeaf) {
+            Vector<OctTreeNode> vecResultOctTreeNode = new Vector<OctTreeNode>();
+            vecResultOctTreeNode.add(this);
+            return vecResultOctTreeNode;
+        }
+        else {
+            OctTreeNode next = null;
+            for (int i = 0; i < children.length; i++) {
+                OctTreeNode current = children[i];
+                Pair<Comparable, Comparable> minMaxCol1 = ((Pair<Comparable, Comparable>)(current.hMinMaxPerColumn.get(colNamesDatatypes[0][0])));
+                Pair<Comparable, Comparable> minMaxCol2 = ((Pair<Comparable, Comparable>)(current.hMinMaxPerColumn.get(colNamesDatatypes[1][0])));
+                Pair<Comparable, Comparable> minMaxCol3 = ((Pair<Comparable, Comparable>)(current.hMinMaxPerColumn.get(colNamesDatatypes[2][0])));
+                boolean cond1 = ((Comparable)minMaxCol1.val1).compareTo(valCol1) <= 0 && ((Comparable)minMaxCol2.val1).compareTo(valCol2) <= 0 && ((Comparable)minMaxCol3.val1).compareTo(valCol3) <= 0;
+                boolean cond2 = ((Comparable)minMaxCol1.val2).compareTo(valCol1) >= 0 && ((Comparable)minMaxCol2.val2).compareTo(valCol2) >= 0 && ((Comparable)minMaxCol3.val2).compareTo(valCol3) >= 0;
+                if (cond1 && cond2) {
+                    next = current;
+                }
+            }
+            if (next == null) {
+                return new Vector<OctTreeNode>(); // Empty list of keys
+            }
+            return next.searchThreeColumns(valCol1,valCol2,valCol3);
+        }
+    }
+
+    /*
+    If OctTreeNode is a leaf:
+       1. Return the current OctTreeNode in a vector
+    If OctTreeNode is a non-leaf:
+       1. Decide which child OctTreeNode(s) to go next.
+    */
+    public Vector<OctTreeNode> searchTwoColumns (Pair<String, Comparable> col1,Pair<String, Comparable> col2) {
+        if (isLeaf) {
+            Vector<OctTreeNode> vecResultOctTreeNode = new Vector<OctTreeNode>();
+            vecResultOctTreeNode.add(this);
+            return vecResultOctTreeNode;
+        }
+        else {
+            Vector<OctTreeNode> vecNextOctTreeNodes = new Vector<>();
+            for (int i = 0; i < children.length; i++) {
+                OctTreeNode current = children[i];
+                Pair<Comparable,Comparable> minMaxCol1 = ((Pair<Comparable, Comparable>)(current.hMinMaxPerColumn.get(col1.val1)));
+                Pair<Comparable,Comparable> minMaxCol2 = ((Pair<Comparable, Comparable>)(current.hMinMaxPerColumn.get(col2.val1)));
+                boolean cond1 = ((Comparable)minMaxCol1.val1).compareTo(col1.val2) <= 0 && ((Comparable)minMaxCol2.val1).compareTo(col2.val2) <= 0;
+                boolean cond2 = ((Comparable)minMaxCol1.val2).compareTo(col1.val2) >= 0 && ((Comparable)minMaxCol2.val2).compareTo(col2.val2) >= 0;
+                if (cond1 && cond2) {
+                    vecNextOctTreeNodes.add(current);
+                }
+            }
+            Vector<OctTreeNode> vecResultOctTreeNodes = new Vector<OctTreeNode>();
+            for (OctTreeNode next : vecNextOctTreeNodes) {
+                Vector<OctTreeNode> vecTmpOctTreeNodes = next.searchTwoColumns(col1,col2);
+                for(OctTreeNode OctTreeNode : vecTmpOctTreeNodes) {
+                    vecResultOctTreeNodes.add(OctTreeNode);
+                }
+            }
+            return vecResultOctTreeNodes;
+        }
+    }
+    /*
+    If OctTreeNode is a leaf:
+       1. Return the current OctTreeNode in a vector
+    If OctTreeNode is a non-leaf:
+       1. Decide which child OctTreeNode(s) to go next.
+     */
+    public Vector<OctTreeNode> searchOneColumn (Pair<String, Comparable> col) {
+        if (isLeaf) {
+            Vector<OctTreeNode> vecResultOctTreeNode = new Vector<OctTreeNode>();
+            vecResultOctTreeNode.add(this);
+            return vecResultOctTreeNode;
+        }
+        else {
+            Vector<OctTreeNode> vecNextOctTreeNode = new Vector<>();
+            for (int i = 0; i < children.length; i++) {
+                OctTreeNode current = children[i];
+                Pair<Comparable,Comparable> minMaxCol = ((Pair<Comparable,Comparable>)(current.hMinMaxPerColumn.get(col.val1)));
+                boolean cond1 = ((Comparable)minMaxCol.val1).compareTo(col.val2) <= 0;
+                boolean cond2 = ((Comparable)minMaxCol.val2).compareTo(col.val2) >= 0;
+                if (cond1 && cond2) {
+                    vecNextOctTreeNode.add(current);
+                }
+            }
+
+            Vector<OctTreeNode> vecResultOctTreeNodes = new Vector<OctTreeNode>();
+            for (OctTreeNode next : vecNextOctTreeNode) {
+                Vector<OctTreeNode> vecTmpKeys = next.searchOneColumn(col);
+                for(OctTreeNode OctTreeNode : vecTmpKeys) {
+                    vecResultOctTreeNodes.add(OctTreeNode);
+                }
+            }
+            return vecResultOctTreeNodes;
+        }
+    }
 }
